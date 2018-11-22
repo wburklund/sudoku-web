789c4dc9b4108bc674ee1c77af169ed705f4db4d
Will Burklund <waburklund@gmail.com>
2018-11-21 20:31:57 -0600

    Add board building code

---
+++ game.js
@@ -0,0 +1,14 @@
+function buildBoardHTML() {
+    let boardHTML = '<tbody>';
+    for (let y = 0; y < 9; y++) {
+        boardHTML += '<tr>';
+        for (let x = 0; x < 9; x++) {
+            boardHTML += '<td></td>';
+        }
+        boardHTML += '</tr>';
+    }
+    boardHTML += '</tbody>';
+    return boardHTML;
+}
+
+document.querySelector('#sudokuBoard').innerHTML = buildBoardHTML();
