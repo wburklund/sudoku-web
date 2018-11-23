d3684902aff956b90bb5cacf59884ab21bc82eb5
Will Burklund <waburklund@gmail.com>
2018-11-23 12:46:48 -0600

    Move conflict checking to own file

---
+++ conflicts.js
@@ -0,0 +1,55 @@
+function updateConflicts(state) {
+    const board = state.board;
+    board.forEach(cell => {
+        if (cell.type === 'conflict') {
+            cell.type = 'normal';
+        }
+    });
+    for (let i = 0; i < 9; i++) {
+        _markConflictsForRow(board, i);
+        _markConflictsForColumn(board, i);
+        _markConflictsForBox(board, i);
+    }
+}
+
+function _markConflictsForRow(board, row) {
+    const startCellIndex = 9 * row;
+    const cellRow = board.slice(startCellIndex, startCellIndex + 9);
+    _markConflicts(cellRow);
+}
+
+function _markConflictsForColumn(board, column) {
+    let cellColumn = [];
+    for (let row = 0; row < 9; row++) {
+        cellColumn.push(board[9 * row + column]);
+    }
+    _markConflicts(cellColumn);
+}
+
+function _markConflictsForBox(board, box) {
+    const startRow = Math.floor(box / 3) * 3;
+    const endRow = startRow + 2;
+    const startColumn = (box * 3) % 9;
+    const endColumn = startColumn + 2;
+
+    const cellBox = board.filter((cell, index) => {
+        return index >= startRow && index <= endRow
+            && index >= startColumn && index <= endColumn;
+    });
+
+    _markConflicts(cellBox);
+}
+
+function _markConflicts(cellSet) {
+    const values = cellSet.map(c => c.value);
+
+    const conflicts = sudoku.DIGITS
+        .split('')
+        .filter(d => values.indexOf(d) !== values.lastIndexOf(d));
+
+    cellSet.forEach((c, index) => {
+        if (conflicts.includes(c.value) && c.type !== 'given') {
+            c.type = 'conflict';
+        }
+    });
+}
