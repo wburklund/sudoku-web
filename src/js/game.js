d53be0a578f475ece83a0810106b74d634c5fa45
Will Burklund <waburklund@gmail.com>
2018-12-05 22:33:23 -0600

    Add notes to layout

--- src/js/game.js
+++ src/js/game.js
@@ -12,9 +12,7 @@ const buildGridHTML = () => {
   for (let y = 0; y < 9; y += 1) {
     gridHTML += '<tr>';
     for (let x = 0; x < 9; x += 1) {
-      gridHTML += '<td class="cell">';
-      gridHTML += '<input type="number">';
-      gridHTML += '</td>';
+      gridHTML += '<td class="cell"></td>';
     }
     gridHTML += '</tr>';
   }
@@ -22,19 +20,40 @@ const buildGridHTML = () => {
   return gridHTML;
 };
 
+const buildCellHTML = () => {
+  let cellHTML = '<input type="number">';
+  cellHTML += '<div class="cell-notes">';
+
+  for (let i = 1; i < 10; i += 1) {
+    if (i % 3 === 1) {
+      cellHTML += '<p>';
+    }
+
+    cellHTML += `<span>${i}</span>`;
+
+    if (i % 3 === 0) {
+      cellHTML += '</p>';
+    }
+  }
+
+  cellHTML += '</div>';
+  return cellHTML;
+};
+
 const init = () => {
   // Create the 'sudokuGrid' table
   document.querySelector('#sudokuGrid').innerHTML = buildGridHTML();
 
-  // Setup each cell
   const cells = [...document.querySelectorAll('#sudokuGrid td')];
 
+  // Layout is starting to take longer, so do the cells last
   for (let i = 0; i < cells.length; i += 1) {
     cells[i].id = `c${i}`;
+    cells[i].innerHTML = buildCellHTML();
     // add id/listeners to child input
     cells[i].childNodes[0].id = `i${i}`;
     cells[i].childNodes[0].className = 'cell-input';
-    cells[i].childNodes[0].addEventListener('keydown', onInputKeydown); // navigation/deletes
+    cells[i].childNodes[0].addEventListener('keydown', onInputKeydown);
   }
 
   render();
