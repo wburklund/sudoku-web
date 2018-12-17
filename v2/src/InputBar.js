01d8a2fe97163808c8fcaa8289d7e13854609554
Will Burklund <waburklund@gmail.com>
2018-12-16 19:34:55 -0600

    Add license notices

--- v2/src/InputBar.js
+++ v2/src/InputBar.js
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) Will Burklund. All rights reserved. Licensed under the GPLv3 license.
+    See LICENSE file in the project root for full license information.
+*/
+
 import React, { Component } from 'react';
 
 class InputBar extends Component {
