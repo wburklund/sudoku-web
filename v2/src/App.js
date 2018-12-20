89ba6344b37c490ce7680b59eac32040c3239baa
Will Burklund <waburklund@gmail.com>
2018-12-20 11:14:38 -0600

    Add comments to App.js

--- v2/src/App.js
+++ v2/src/App.js
@@ -13,28 +13,42 @@ import * as lib from './lib';
 class App extends Component {
   constructor(props) {
     super(props);
-    const newGame = lib.newGame('medium');
+    const grid = lib.newGame('medium');  // Todo: replace with "load or generate" helper
     this.state = {
-      controls: {
-        hint: false,
+      controls: {         // ControlBar selections
+        hint: false,        // Whether the player wants a hint
       },
-      grid: newGame,
-      input: {
-        digit: null,
-        noteEnable: false,
+      grid,               // Sudoku grid
+      input: {            // InputBar selections
+        digit: null,        // Selected digit
+        noteEnable: false,  // Whether to enter the selected digit as a note
       },
     };
   }
 
+  /*
+    Input handler called when a non-given cell is clicked.
+
+    Determines whether any special input is desired (hint, note, etc),
+    and handles it appropriately. Otherwise, enters selected digit as cell's value.
+  */
   handleCellClick(index) {
     // Todo: add note support
+
+    // Copy the grid; will need a helper function for deep copy when notes are added
     let grid = this.state.grid.slice();
+
     if (this.state.controls.hint) {
+      // If the player has requested a hint, set the cell's value with the correct digit
       grid[index].value = lib.getHint(grid, index);
+      // Disable hints until the player requests another
       this.setState({ controls: {...this.state.controls, hint: false} });
     } else {
+      // Otherwise, this was normal input. Set the cell's value with the selected digit.
       grid[index].value = this.state.input.digit;
     }
+    // Todo: conflict detection, saving, etc here
+    // Update grid
     this.setState({ grid });
   }
 
