8dc0c27de265906d5542380e8c219cb35876c1c4
Will Burklund <waburklund@gmail.com>
2018-12-20 10:32:18 -0600

    Add comments to lib.js

--- v2/src/lib.js
+++ v2/src/lib.js
@@ -7,25 +7,51 @@ import sudoku from './generator/sudoku';
 
 const emptyCell = {type: 'normal', value: null};
 
+/*
+  Generate a new Sudoku puzzle with the given difficulty.
+
+  The generator returns a puzzle as an 81-character string with either a digit
+  or a period to represent an empty cell. (example: "3..4.6.21" ...)
+  All digits in a new game are made into "given" cells that cannot be modified.
+  Empty cells are "normal" and can be either given a digit or given
+  player-entered "notes" which record possible digits in a cell.
+*/
 export const newGame = (difficulty) => {
-  return sudoku.generate(difficulty)
+  // Generator string -> array of characters -> array of cells
+  return sudoku.generate(difficulty) 
     .split('')
-    .map(value => value === '.' ? Object.assign({}, emptyCell) : {type: 'given', value});
+    .map(value => value === '.' ? {...emptyCell} : {type: 'given', value});
 };
 
+/*
+  Get the correct digit for the requested cell.
+
+  The generator solves the entire puzzle,
+  and the digit at the requested index is returned.
+*/
 export const getHint = (grid, index) => {
+  // Convert our grid back to the generator string format,
+  // throwing out everything that wasn't given by the generator
   const gridString = grid.map(cell => cell.type === 'given' ? cell.value : '.')
     .join('');
 
+  // Solve the grid
   const solvedGrid = sudoku.solve(gridString);
 
+  // Return the digit at the given index
   return solvedGrid[index];
 }
 
+/*
+  Save the game to localStorage.
+*/
 export const saveGame = (state) => {
   localStorage.setItem('sudokuGame', JSON.stringify(state));
 };
 
+/*
+  Load the game from localStorage.
+*/
 export const loadGame = () => {
   return JSON.parse(localStorage.getItem('sudokuGame'));
 };
