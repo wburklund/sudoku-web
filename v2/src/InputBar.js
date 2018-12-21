c6e8eb25da2886b86314fcd97fc08a6d438c206a
Will Burklund <waburklund@gmail.com>
2018-12-20 18:03:11 -0600

    Style tweaks

--- v2/src/InputBar.js
+++ v2/src/InputBar.js
@@ -4,29 +4,30 @@
 */
 
 import React, { Component } from 'react';
+import styles from './InputBar.module.css';
 
 class InputBar extends Component {
     render() {
         return (
             <React.Fragment>
                 <button 
-                    className={'cell'}
+                    className={styles.cell}
                     onClick={() => this.props.onDigitSelect(null)}
                     style={{color: 'transparent', marginRight: '16px'}}
                 >
                     E
                 </button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(1)}>1</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(2)}>2</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(3)}>3</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(4)}>4</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(5)}>5</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(6)}>6</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(7)}>7</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(8)}>8</button>
-                <button className={'cell'} onClick={() => this.props.onDigitSelect(9)}>9</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(1)}>1</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(2)}>2</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(3)}>3</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(4)}>4</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(5)}>5</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(6)}>6</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(7)}>7</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(8)}>8</button>
+                <button className={styles.cell} onClick={() => this.props.onDigitSelect(9)}>9</button>
                 <button 
-                    className={'cell'} 
+                    className={styles.cell} 
                     onClick={this.props.onNoteToggle}
                     style={{marginLeft: '16px'}}
                 >
