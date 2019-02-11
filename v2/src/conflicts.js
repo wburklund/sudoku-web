a70ef6441e12c85d86c0e851b2d317bda18c9196
Will Burklund <waburklund@gmail.com>
2019-02-10 21:05:38 -0600

    Store cellSets in an array instead of generating repeatedly

--- v2/src/conflicts.js
+++ v2/src/conflicts.js
@@ -5,40 +5,40 @@
 
 import { deepCopyGrid } from "./util";
 
-/*
-    Generator that yields interacting sets of Sudoku cells. Sets represented as arrays of indices.
-*/
-function *cellSets() {
-    // Yield all horizontal lines
+// Sets of interacting cells (rows, columns, boxes)
+let cellSets;
+
+// Creates cellSets array; runs once
+const createCellSets = () => {
+    cellSets = [];
+
     for (let row = 0; row < 9; row++) {
         let set = [];
         for (let col = 0; col < 9; col++) {
             set.push((row * 9) + col);
         }
-        yield set;
+        cellSets.push(set);
     }
-
-    // Yield all vertical lines
+    
     for (let col = 0; col < 9; col++) {
         let set = [];
         for (let row = 0; row < 9; row++) {
             set.push((row * 9) + col);
         }
-        yield set;
+        cellSets.push(set);
     }
-
-    // Yield all boxes (top left -> top middle -> top right -> middle left -> ...)
+    
     for (let box = 0; box < 9; box++) {
         let startCol = (box % 3) * 3;
         let startRow = 3 * Math.floor(box / 3);
-
+    
         let set = [];
         for (let row = startRow; row < startRow + 3; row++) {
             for (let col = startCol; col < startCol + 3; col++) {
                 set.push((row * 9) + col);
             }
         }
-        yield set;
+        cellSets.push(set);
     }
 }
 
@@ -46,10 +46,14 @@ function *cellSets() {
     Takes a grid as input, and returns a grid with the conflicting cells marked.
 */
 const updateConflicts = (grid) => {
+    if (!cellSets) {
+        createCellSets();
+    }
+
     let conflictCells = new Set();
 
     // Run through each interacting set of cells.
-    for (let set of cellSets()) {
+    for (let set of cellSets) {
         const cellValues = set.map(index => grid[index].value);
 
         // Get an array of digits that occur more than once in the set.
